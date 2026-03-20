013549756604994bc2f0b83d1235f3766a562e80
kimhyun <doriandko9431@gmail.com>
2026-03-20 11:25:51 +0900

    Step09 추가

--- Step01_DataType.py
+++ Step01_DataType.py
@@ -2,12 +2,15 @@
 
 """
     여기서부터 주석입니다.
-    어쩌구 저쩌구 블라블라..
+    어쩌구 저쩌구 블라블라...
+
+    이것은 여러 줄 주석입니다. 
 """
 
 print("Step01 시작")
 
 # python의 여러가지 데이터 type에 대해서 알아보자 
+num1 = 10
 
 # int type(정수)
 num1 = 10
@@ -17,6 +20,9 @@ num2 = 10.1
 # str type
 myName = '김현경'
 yourName = "아톰"
+
+
+
 ourName = """
     KT Cloud Infra    
     화이팅!
@@ -24,22 +30,30 @@ ourName = """
 
 # 순서가 중요한 여러개의 데이터를 관리하고 싶다면...
 # 내가 좋아하는 음식 목록 3가지를 하나의 변수에 순서대로 담고 싶다면...
-food1 = "떡볶이" 
-food2 = "미더덕"
-food3 = "곱창"
-foods = ["삼겹살","김밥","닭발"]  #List type
 
-print(foods)
+# food1 = "떡볶이" 
+# food2 = "미더덕"
+# food3 = "곱창"
+
+foods = ["떡볶이","미더덕","곱창"]  #List type
+
+print(foods[0])
+print(foods[1])
+print(foods[2])
+
 
 # 순서가 중요치 않지만 여러개의 데이터를 관리하고 싶다면...
 # 회원 1명의 정보
-num=1
-name="김구라"
-addr="노량진"
+
+# num=1
+# name="김구라"
+# addr="노량진"
 
 mem1 = {"num":1, "name":"김구라", "addr":"노량진"} #dict type
 
-print(mem1)
+print(mem1["num"])
+print(mem1["name"])
+print(mem1["addr"])
 
 
 # 순서가 중요치 않은 데이터를 하나의 묶음으로 관리(키값 없이)
@@ -48,8 +62,7 @@ mySet = {"빨강사탕", "초록사탕", "노랑사탕"} #Set Type
 print(mySet)
 
 # 특정 Code 블럭을 필요한 시점에 일괄 실행하고 싶다면?
-
-#함수를 만들고
+# 함수를 만들고
 def store():
     print("냉장고 문을 연다")
     print("물건을 저장한다")
@@ -68,7 +81,7 @@ a = "테스트"
 
 
 # 참과 거짓을 나타내는 data type (Bool)
-isMan=True
-isWoman=False
-isDifferent=True
-isRun=False
+isMan = True
+isWoman = False
+isDifferent = True
+isRun = False
---
+++ Step10_Main.py
@@ -0,0 +1,8 @@
+# Step10_main.py
+
+'''
+    현재 파일은 즉 Step10_Main.py 에서 run 해서 실행하면
+    
+'''
+
+print(__name__)
